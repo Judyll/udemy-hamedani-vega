efab3f3a7de55d58a971d0b2eb1a94e7aa2fd55a
Judyll Agan <judyll.agan@gmail.com>
2019-01-09 07:54:46 +0800

    #34 Adding a new angular component

--- VegaApp-SPA/src/app/app.module.ts
+++ VegaApp-SPA/src/app/app.module.ts
@@ -1,11 +1,15 @@
+// Node modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Local components
 import { AppComponent } from './app.component';
+import { VehicleDetailsComponent } from './vehicle/vehicle-details/vehicle-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehicleDetailsComponent
   ],
   imports: [
     BrowserModule
